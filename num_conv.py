53f0207e7f4cd1f4602237aef6d9e0c97633f13e
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-04-05 18:57:34 +0000

    num_conv_updated.py

--- num_conv.py
+++ num_conv.py
@@ -3,15 +3,16 @@
 # Date: Apr 5, 2025
 # This program that compare 3 numbers from smallest to biggest
 
+
 def main():
     # Display welcoming message
     print("Hello! Welcome to this program!")
     # Get user input of the first number as a string
-    first_number_string = input ("Please enter the fist number as an integer: ")
+    first_number_string = input("Please enter the fist number as an integer: ")
     # Get user input of the second number as a string
-    second_number_string = input ("Please enter the Second number as an integer: ")
-    #Get user input of the third number as a string
-    third_number_string = input ("Please enter the third number as integer: ")
+    second_number_string = input("Please enter the Second number as an integer: ")
+    # Get user input of the third number as a string
+    third_number_string = input("Please enter the third number as integer: ")
     # try catch statement
     try:
         # Casting number 1 into integer
@@ -22,7 +23,11 @@ def main():
         third_number = int(third_number_string)
 
         # Check if any numbers are equal
-        if first_number == second_number or first_number == third_number or second_number == third_number:
+        if (
+            first_number == second_number
+            or first_number == third_number
+            or second_number == third_number
+        ):
             print("Some numbers are equal.")
 
         # Comparing number 1 with number 2 and 3
@@ -30,28 +35,52 @@ def main():
             # If above is true, comparing number 2 and 3
             if second_number < third_number:
                 # Display the result from smallest to largest
-                print("The number order is: {} < {} < {}".format(first_number, second_number, third_number))
+                print(
+                    "The number order is: {} < {} < {}".format(
+                        first_number, second_number, third_number
+                    )
+                )
             else:
                 # Display the result from smallest to largest
-                print("The num order is: {} < {} < {}".format(first_number, third_number, second_number))
+                print(
+                    "The num order is: {} < {} < {}".format(
+                        first_number, third_number, second_number
+                    )
+                )
         else:
             # compare number 2 with number 1 and 3
             if second_number < first_number and second_number < third_number:
                 # Compare number 1 with number 3
                 if first_number < third_number:
                     # Display the result from smallest to largest
-                    print("The num order is: {} < {} < {}".format(second_number, first_number, third_number))
+                    print(
+                        "The num order is: {} < {} < {}".format(
+                            second_number, first_number, third_number
+                        )
+                    )
                 else:
                     # Display the result from smallest to largest
-                    print("The num order is: {} < {} < {}".format(second_number, third_number, first_number))
+                    print(
+                        "The num order is: {} < {} < {}".format(
+                            second_number, third_number, first_number
+                        )
+                    )
             else:
                 # Compare number 1 with number 2
                 if first_number < second_number:
                     # Display the result from smallest to largest
-                    print("The number order is: {} < {} < {}".format(third_number, first_number, second_number))
+                    print(
+                        "The number order is: {} < {} < {}".format(
+                            third_number, first_number, second_number
+                        )
+                    )
                 else:
                     # Display the result from smallest to largest
-                    print("The number is: {} < {} < {}".format(third_number, second_number, first_number))
+                    print(
+                        "The number is: {} < {} < {}".format(
+                            third_number, second_number, first_number
+                        )
+                    )
             # Show the smallest and largest number
         smallest = min(first_number, second_number, third_number)
         largest = max(first_number, second_number, third_number)
@@ -68,6 +97,6 @@ def main():
         print("Invalid input, please try again.")
     print("Thank you for using the program!")
 
+
 if __name__ == "__main__":
     main()
-
