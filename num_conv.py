a9385c7a6c9d28e52a2f3d1914a66f96e0319da4
Xiaohan Tian <xiaohan.tian@stu.ocsb.ca>
2025-04-10 00:37:30 +0000

    num_conv_updated.py

--- num_conv.py
+++ num_conv.py
@@ -22,79 +22,93 @@ def main():
         # Casting number 3 into integer
         third_number = int(third_number_string)
 
+        # Check if all numbers are equal
+        numbers_are_equal = False
+
         # Check if any numbers are equal
-        if (
+        if first_number == second_number == third_number:
+            print("All numbers are equal.")
+        # Some numbers are equal
+        elif (
             first_number == second_number
             or first_number == third_number
             or second_number == third_number
         ):
             print("Some numbers are equal.")
 
-        # Comparing number 1 with number 2 and 3
-        if first_number < second_number and first_number < third_number:
-            # If above is true, comparing number 2 and 3
-            if second_number < third_number:
-                # Display the result from smallest to largest
-                print(
-                    "The number order is: {} < {} < {}".format(
-                        first_number, second_number, third_number
-                    )
-                )
-            else:
-                # Display the result from smallest to largest
-                print(
-                    "The num order is: {} < {} < {}".format(
-                        first_number, third_number, second_number
-                    )
-                )
-        else:
-            # compare number 2 with number 1 and 3
-            if second_number < first_number and second_number < third_number:
-                # Compare number 1 with number 3
-                if first_number < third_number:
-                    # Display the result from smallest to largest
-                    print(
-                        "The num order is: {} < {} < {}".format(
-                            second_number, first_number, third_number
-                        )
-                    )
-                else:
-                    # Display the result from smallest to largest
-                    print(
-                        "The num order is: {} < {} < {}".format(
-                            second_number, third_number, first_number
-                        )
-                    )
-            else:
-                # Compare number 1 with number 2
-                if first_number < second_number:
+        # Check the numbers are equal or not
+        numbers_are_equal = True
+
+        # where numbers are not equal
+        if not numbers_are_equal:
+            # Comparing number 1 with number 2 and 3
+            if first_number < second_number and first_number < third_number:
+                # If above is true, comparing number 2 and 3
+                if second_number < third_number:
                     # Display the result from smallest to largest
                     print(
                         "The number order is: {} < {} < {}".format(
-                            third_number, first_number, second_number
+                            first_number, second_number, third_number
                         )
                     )
                 else:
                     # Display the result from smallest to largest
                     print(
-                        "The number is: {} < {} < {}".format(
-                            third_number, second_number, first_number
+                        "The num order is: {} < {} < {}".format(
+                            first_number, third_number, second_number
                         )
                     )
-            # Show the smallest and largest number
+            else:
+                # compare number 2 with number 1 and 3
+                if second_number < first_number and second_number < third_number:
+                    # Compare number 1 with number 3
+                    if first_number < third_number:
+                        # Display the result from smallest to largest
+                        print(
+                            "The num order is: {} < {} < {}".format(
+                                second_number, first_number, third_number
+                            )
+                        )
+                    else:
+                        # Display the result from smallest to largest
+                        print(
+                            "The num order is: {} < {} < {}".format(
+                                second_number, third_number, first_number
+                            )
+                        )
+                else:
+                    # Compare number 1 with number 2
+                    if first_number < second_number:
+                        # Display the result from smallest to largest
+                        print(
+                            "The number order is: {} < {} < {}".format(
+                                third_number, first_number, second_number
+                            )
+                        )
+                    else:
+                        # Display the result from smallest to largest
+                        print(
+                            "The number is: {} < {} < {}".format(
+                                third_number, second_number, first_number
+                            )
+                        )
+        # Show the smallest and largest number
         smallest = min(first_number, second_number, third_number)
         largest = max(first_number, second_number, third_number)
+
         # Display the smallest and largest number
         print("The smallest number is: {}".format(smallest))
         print("The largest number: {}".format(largest))
 
-        # Calculate and show the average
+        # Calculate and show the average and save it for 2 decimal places
         average = (first_number + second_number + third_number) / 3
         print("The average of the numbers is: {:.2f}".format(average))
 
     except Exception:
         # Exceptions where users did not input a number as integer
         print("Invalid input, please try again.")
+
+    # ending message
     print("Thank you for using the program!")
 
 
